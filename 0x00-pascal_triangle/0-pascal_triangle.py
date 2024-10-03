ff98874474abc405cd134007ddd1c6d2ec740005
mhlibya <mhlibya728@gmail.com>
2024-10-04 01:06:00 +0200

    0x00-pascal_triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+def pascal_row(row_num, previous_row):
+    row = [1] * row_num
+    for j in range(1, row_num - 1):
+        row[j] = previous_row[j - 1] + previous_row[j]
+    return row
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    lists = [[1]]
+    print(lists[0])
+    
+    for i in range(2, n + 1):
+        row = pascal_row(i, lists[i - 2])
+        print(row)
+        lists.append(row)
+    
+    return lists
+
+if __name__ == "__main__":
+    n = int(input("n: "))
+    pascal_triangle(n)
