2543befefb1e7b333f53a4467935313bbd0f2a96
mhlibya <mhlibya728@gmail.com>
2024-10-24 08:10:02 +0200

    0x03-log_parsing

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,20 +5,18 @@
 """
 
 def canUnlockAll(boxes):
-    """
-       checks if all the boxes are openable
-    """
+    
     if (type(boxes)) is not list:
         return False
     elif (len(boxes)) == 0:
         return False
     
-    for k in range(1, len(boxes) - 1):
-        boxes_checked = False
-        for idx in range(len(boxes)):
-            boxes_checked = k in boxes[idx] and k != idx
-            if boxes_checked:
-                break
-        if boxes_checked is False:
-            return boxes_checked
+    keys = boxes[0]
+    for key in keys:
+        for index in boxes[key]:
+            if index not in keys:
+                keys.append(index)
+    for i in range(1, len(boxes)):
+        if i not in keys:
+            return False
     return True
