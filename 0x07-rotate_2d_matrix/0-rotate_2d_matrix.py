8de406283b802ba7168259b7bfc2ee56a3cf1ad2
mhlibya <mhlibya728@gmail.com>
2024-11-19 12:53:04 +0200

    0x07-rotate_2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,10 +3,14 @@
 0x07-rotate_2d_matrix
 """
 def rotate_2d_matrix(matrix):
+    """
+    rotate 2d matrix
+    """
     n = len(matrix) - 1
     r = (n + 1) // 2 if n % 2 != 0 else n // 2
-
+#   rotating the matrix in r steps
     for i in range(r):
+#       spacifing the sides of the matrix and rotate them
         top = matrix[i][i : n + 1 - i]
         right = [matrix[j][n - i] for j in range(i, n + 1 - i)]
         bottom = matrix[n - i][i : n + 1 - i][::-1]
