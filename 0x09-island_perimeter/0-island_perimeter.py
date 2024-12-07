0189235e71d079913d5313e9f3b5368ce554ffab
mhlibya <mhlibya728@gmail.com>
2024-12-07 15:45:12 +0200

    0x09-island_perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,6 +2,8 @@
 """
 0-island_perimeter
 """
+
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of an island represented in a grid.
@@ -15,7 +17,7 @@ def island_perimeter(grid):
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0]) if rows > 0 else 0
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:  # Check if it's land
@@ -28,5 +30,5 @@ def island_perimeter(grid):
                     perimeter += 1
                 if j == cols - 1 or grid[i][j + 1] == 0:  # Right
                     perimeter += 1
-    
+
     return perimeter
